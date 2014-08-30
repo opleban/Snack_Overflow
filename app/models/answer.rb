b7726de80d496d529a80a1fc71324fce84ead4d7
Ori <opleban@gmail.com>
2014-08-29 22:32:20 -0700

    Adds has_and_belongs_to_many association between tags and questions

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ActiveRecord::Base
-    belongs_to :user
-    belongs_to :question
+  belongs_to :user
+  belongs_to :question
 	has_many :comments, as: :feedback
 
 	validates :body, :presence => true
