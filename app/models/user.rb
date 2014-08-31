c2ce135fb3501f862150ec380484c1f10365455b
plauer <plauer411@gmail.com>
2014-08-30 18:35:32 -0700

    requires email to be unique in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
   validates :email, presence:true
   validates :username, presence:true
   validates :password, presence:true
+  validates_uniqueness_of :email
 
 end
