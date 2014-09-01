9dae80f672b7bee9bc1dfbe4ac2894a400ca86ff
plauer <plauer411@gmail.com>
2014-08-31 21:21:24 -0700

    signin and signup feature tests

--- spec/features/features_spec.rb
+++ spec/features/features_spec.rb
@@ -12,18 +12,22 @@ end
 
 feature "sign in" do
   scenario "displays response when password too short" do
-    visit '/sessions/new'
+    visit new_session_path
     fill_in "session_password", :with => "x"
     click_on "Sign in"
     expect(page).to have_content "Invalid email/password combination"
   end
 end
 
-feature "grandma's normal response" do
+feature "sign up" do
   scenario "displays angry response when input is capitalized" do
-    visit '/grandma'
-    fill_in "input", :with => "GRANDMA YOU'RE LAME"
-    click_on "Say it!"
-    expect(page).to have_content "Shut the fuck up sonny!"
+    visit new_user_path
+    fill_in "user_name", :with => "Turtle"
+    fill_in "user_username", :with => "TurtleLover405"
+    fill_in "user_email", :with => "turtlemolestor@dbc.com"
+    fill_in "user_password", :with => "iliketurtles"
+
+    click_on "Create my account"
+    expect(page).to have_content "turtlemolestor@dbc.com"
   end
 end
