f7ca751ccf1d86a5b24d05b790a6c4b378648cea
plauer <plauer411@gmail.com>
2014-08-31 21:15:43 -0700

    adds homepage feature test

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,24 +1,29 @@
 require_relative '../rails_helper'
 
-describe "Users pages", :js => true do 
 
-  context "user profile page" do 
-    let(:user) { FactoryGirl.create :user }
-    before { visit user_path(user) }
 
-    # puts page.inspect
-    it { should have_content(user.name)}
-    it { should have_title(user.name) }
+
+feature "homepage" do
+  scenario "displays 'Welcome'" do
+    visit root_path
+    expect(page).to have_content "Welcome to Snack Overflow"
   end
+end
 
-  context "sign up page" do 
-    before { visit new_user_path}
-
-    it {should have_content('Sign up') }
-    it {should have_title(full_title('Sign up')) }
+feature "sign in" do
+  scenario "displays response when password too short" do
+    visit '/sessions/new'
+    fill_in "session_password", :with => "x"
+    click_on "Sign in"
+    expect(page).to have_content "Invalid email/password combination"
   end
+end
 
-
-
-
+feature "grandma's normal response" do
+  scenario "displays angry response when input is capitalized" do
+    visit '/grandma'
+    fill_in "input", :with => "GRANDMA YOU'RE LAME"
+    click_on "Say it!"
+    expect(page).to have_content "Shut the fuck up sonny!"
+  end
 end
