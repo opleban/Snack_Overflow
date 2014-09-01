01e74cabecd540ff0d4e6d3f199790825b0a0cd9
plauer <plauer411@gmail.com>
2014-08-31 21:00:48 -0700

    adds feature tests folder, adds user_spec.rb in features folder

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../rails_helper'
+
+describe "Users pages" do 
+
+  context "user profile page" do 
+    let(:user) { FactoryGirl.create :user }
+    before { visit user_path(user) }
+
+    it { should have_content(user.name)}
+    it { should have_title(user.name) }
+  end
+
+  context "sign up page" do 
+    before { visit new_user_path}
+
+    it {should have_content('Sign up') }
+    it {should have_title(full_title('Sign up')) }
+  end
+
+
+
+
+end
