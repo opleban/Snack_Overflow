af96fd57614b7a07bc91e048349e46aaa4e813fe
Ori <opleban@gmail.com>
2014-09-01 03:04:20 -0700

    removes more git merge comments

--- app/assets/javascripts/questions/show.js
+++ app/assets/javascripts/questions/show.js
@@ -19,26 +19,21 @@ function Answer(JSONObject) {
 
 //CONTROLLER+VIEW TO POPULATE ANSWERS W/ ATTACHED COMMENTS
 function QuestionShowController() {
-  this.question_id = $(location).attr('href').match(/^d/);
-  this.question = questionList.findByID(this.question_id);
+  this.answerList = []
 }
 
 QuestionShowController.prototype = {
 
-<<<<<<< HEAD
-  bindNewAnswerListener: function(event){
-    $(".submit_button").on('click', questionShowController.renderNewAnswerList)
-=======
   renderAnswerList: function() {
     $.ajax({
       type: "GET",
       url: $(location).attr('href')+"/answers",
       dataType: 'HTML'
     }).done(function(partial) {
+      console.log("success")
       $(".answer-list").html(partial)
       qPageController.bindEvents()
     })
->>>>>>> 9a81d1b09017138a23f4f60c8837343983554fe7
   },
 
   renderNewAnswerList: function(event) {
@@ -48,65 +43,11 @@ QuestionShowController.prototype = {
       type: "POST",
       url: $(this).parents("form").attr('action'),
       data: $(this).parents("form").serialize(),
-<<<<<<< HEAD
-      dataType: 'JSON'
-    }).done(function(answerJsonObject) {
-      questionShowController.addAnswerToAnswerList(answerJsonObject)
-      questionShowController.sortAnswerList()
-      questionShowController.displayAnswerList()
-    })
-  },
-
-  addAnswerToAnswerList: function(answerJsonObject) {
-    var answerJSObject = new Answer(answerJsonObject)
-    this.question.questionList.push(answerJSObject)
-  },
-
-  sortAnswerList: function() {
-    this.question.answerList.sort(compare)
-    function compare(a,b) {
-      if (a.score < b.score)
-         return -1;
-      if (a.score > b.score)
-        return 1;
-      return 0;
-    }
-  },
-
-  displayAnswerList: function() {
-    $(".answer_list").append('<h2>There are '+this.question.answerList.length+' answers</h2>')
-    for(i=0;i<this.question.answerList.length;i++) {
-      answerDiv = this.formatAnswerDiv(this.question.answerList[i])
-      $(".answer_list").append(answerDiv)
-    }
-  },
-
-  formatAnswerDiv: function(answer){
-    var thisAnswer = {
-      "body": answer.body,
-      "score": answer.score,
-    }
-    var answerMainTemplate = "<div class='answer_main'>{{body}}</div>";
-    var answerMainHtml = Mustache.to_html(answerMainTemplate, thisAnswer);
-    var answerLeftTemplate = "<div class='answer_left'>{{score}}</div>";
-    var answerLeftHtml = Mustache.to_html(answerLeftTemplate, thisAnswer);
-    var answerContent = [
-                    "<div class='answer-div'>",
-                    answerMainHtml,
-                    answerLeftHtml,
-                    "</div>"
-                  ]
-    return answerContent.join("")
-=======
       dataType: 'HTML'
     }).done(function(partial) {
       console.log("success")
       $(".answer-list").html(partial)
       qPageController.bindEvents()
     })
->>>>>>> 9a81d1b09017138a23f4f60c8837343983554fe7
   }
 }
-
-
-
