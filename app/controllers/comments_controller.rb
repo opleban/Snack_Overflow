872a72cc8825b63a7d34546d24e3010d33e35f23
Ori <opleban@gmail.com>
2014-08-31 19:27:00 -0700

    modifies comment controller for AJAX, now returns JSON

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,16 +8,25 @@ class CommentsController < ApplicationController
   end
 
   def create
-
     @post = post
-    comment = Comment.create(comment_params)
-    @post.comments << comment
-    redirect_to :back
+    user = User.find(session[:user_id])
+    comment = Comment.new(comment_params)
+    comment.user_id = user.id
+    respond_to do |format|
+      if comment.save
+        @post.comments << comment if comment.save
+        format.html { redirect_to :back}
+        format.json { render json: {comment:comment, user:user}, status: :created }
+      else
+        format.html { redirect_to :back, notice: 'Comment was unsuccessfully created.' }
+        format.json { render json: comment.errors.full_messages.join, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body, :user_id)
   end
 
   def post
